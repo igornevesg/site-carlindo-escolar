f8c95b597f133abb426716abd6845bfbd175a317
igornevesg <igornevesmoc@gmail.com>
2020-06-21 22:56:25 -0300

    inicio

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+function abrirMenu() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function escolar() {
+    var mouse = document.getElementsByClassName("foto-criança")
+}
