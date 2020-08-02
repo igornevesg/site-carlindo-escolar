091e9193dfc2f470be355b261670d6b3cd06967f
igornevesg <igornevesmoc@gmail.com>
2020-08-01 23:42:37 -0300

    Alteraçoes responsividade

--- js/menu.js
+++ js/menu.js
@@ -14,5 +14,26 @@ function forward() {
 }
 
 function backward() {
-    document.getElementById("paragrafo").innerHTML = "Paragraph back";   
+    document.getElementById("paragrafo").innerHTML = "Paragraph back";
+}
+
+function myFunction(x) {
+    if (x.matches) {
+        document.getElementById("myTopNav").style.display = 'block';
+    } else {
+        document.getElementById("myTopNav").style.display = 'none';
+    }
+}
+
+var x = window.matchMedia("(min-width: 769px)")
+myFunction(x);
+x.addListener(myFunction);
+
+function abrirMenu() {
+    var submenu = document.getElementById("myTopNav");
+    if (submenu.style.display == 'none') {
+        submenu.style.display = 'block';
+    } else {
+        submenu.style.display = 'none';
+    }
 }
